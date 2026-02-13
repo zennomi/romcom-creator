0163d67fc006818597ff6fe2df0b5276822012f4
Lorant <lorant@once-ui.com>
2026-02-13 15:18:37 +0100

    docs: update config and custom with latest Once UI features

--- src/resources/once-ui.config.js
+++ src/resources/once-ui.config.js
@@ -39,12 +39,12 @@ const fonts = {
 // default customization applied to the HTML in the main layout.tsx
 const style = {
   theme: "system", // dark | light | system
-  neutral: "gray", // sand | gray | slate
+  neutral: "gray", // sand | gray | slate | mint | rose | dusk | custom
   brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   solid: "contrast", // color | contrast | inverse
   solidStyle: "flat", // flat | plastic
-  border: "playful", // rounded | playful | conservative
+  border: "playful", // rounded | playful | conservative | sharp
   surface: "filled", // filled | translucent
   transition: "all", // all | micro | macro
   scaling: "100", // 90 | 95 | 100 | 105 | 110
